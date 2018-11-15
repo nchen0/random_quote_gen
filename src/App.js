8af0d4f8ccc76b47b0d08f957f7bd2d4c8fab939
Nicky Chen <nicky.chen0@gmail.com>
2018-11-14 19:41:33 -0800

    basic function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+
+function getRandom() {
+  return Math.floor(Math.random() * 102);
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+      .then(response => {
+        this.setState({ quotes: response.data.quotes });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
+
+  generateNew = e => {
+    e.preventDefault();
+    console.log("hi");
+    this.setState({});
+  };
+
   render() {
+    let random_number = getRandom();
+    console.log("this.state.quotes length is is: ", this.state.quotes);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="box">
+            {this.state.quotes ? (
+              <div>
+                <p>{this.state.quotes[random_number].quote}</p>{" "}
+                <p>{this.state.quotes[random_number].author}</p>
+              </div>
+            ) : null}
+
+            <button className="btn btn-secondary" onClick={this.generateNew}>
+              New Quote
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
